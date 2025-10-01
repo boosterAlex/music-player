4f1bb9cd8845f875bd619a85bad54c7d04c00082
boosterAlex <alex.booster.com@gmail.com>
2025-10-01 17:28:20 +0300

    add state and fetch

--- music-player/src/App.tsx
+++ music-player/src/App.tsx
@@ -1,13 +1,36 @@
-import { useState } from "react"
-
-const tracks = [
-{id: 1, name: 'Sountrack', url: 'https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3'}, 
-{id: 2, name: 'Soundtrack instrumental', url: 'https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3'}
-]
+import { useEffect, useState } from "react"
 
 export function App() {
 
+  const [tracks, setTracks] = useState(null)
   const [selectedTrackId, setSelectedTrackId ] = useState<null | number>(null)
+
+  useEffect(()=>{
+     fetch('https://musicfun.it-incubator.app/api/1.0/playlists/tracks', {
+        headers: {
+          'api-key': '5ee87017-690c-4fb4-aa8c-7e21225d89b1'
+        }
+     }).then(res => res.json()).then(res=> setTracks(res.data))
+  }, [])
+
+
+  if(tracks === null) {
+    return (
+      <>
+        <h1>Music Player</h1>
+        <h2>Loading...</h2>
+      </>
+    )
+  }
+
+  if(tracks.length === 0) {
+    return (
+      <>
+        <h1>Music Player</h1>
+        <h2>No tracks</h2>
+      </>
+    )
+  }
 
   return (
     <div>
@@ -15,13 +38,13 @@ export function App() {
       <button onClick={()=>{setSelectedTrackId(null)}}>Reset selection</button>
         <ul>
           {tracks.map((track)=>{
-           return <li key={track.name} style={{
+           return <li key={track.id} style={{
             border: track.id === selectedTrackId ? '1px solid white' : 'none' 
            }}>
                    <div onClick={()=>{
             setSelectedTrackId(track.id)
-           }} >{track.name}</div>
-                   <audio src={track.url} controls></audio>
+           }} >{track.attributes.title}</div>
+                   <audio src={track.attributes.attachments[0].url} controls></audio>
                   </li> 
   }) }
         </ul>
